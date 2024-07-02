41ba15031b38ea073fb5a0f57cd6915150eb5ed7
qzhong <12ff54e@gmail.com>
2024-07-02 13:43:07 +0800

    Distinguish arguments and options (begin with '-')

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ Given poloidal magnetic flux $\psi_{p}$ (analytic or numeric), calculate its Boo
 # configure & build
 cmake -B ./build -DCMAKE_BUILD_TYPE:STRING=Release
 cmake --build ./build
-# convert specified gfile to spdata.dat, options are optional except '--input-path'
-./build/mct --input-path path/to/gfile --output-path path/to/spdata.dat --radial 129 --poloidal 255 --use-si
+# convert specified gfile to spdata.dat, options are optional
+./build/mct path/to/gfile --output-path path/to/spdata.dat --radial 129 --poloidal 255 --use-si
 ```
 
 ## Known Issue
--- src/include/clap.h
+++ src/include/clap.h
@@ -1,9 +1,14 @@
+/*
+ * Command Line Argument Parser
+ */
+
 #ifndef ZQ_CLAP
 #define ZQ_CLAP
 
 #include <sstream>
 #include <type_traits>
 #include <unordered_map>
+#include <vector>
 
 #define TYPE_LIST()        \
     PROCESS_TYPE(bool)     \
@@ -23,17 +28,22 @@ struct CLAP {
     using base = T;
     using string = std::string;
 
-    static auto& get_parameter_map() {
+    static auto& get_options() {
         static std::unordered_map<std::string,
                                   std::pair<std::size_t, TYPE_CODE>>
             parameter_map{};
         return parameter_map;
     }
 
+    static auto& get_arguments() {
+        static std::vector<std::pair<std::size_t, TYPE_CODE>> arguments;
+        return arguments;
+    }
+
     template <typename Arg>
     static TYPE_CODE get_type_code() {
 #define PROCESS_TYPE(TYPE) \
-    if constexpr (std::is_same_v<Arg, TYPE>) { return TYPE_CODE::TYPE##_t; }
+    if (std::is_same<Arg, TYPE>::value) { return TYPE_CODE::TYPE##_t; }
         TYPE_LIST()
 #undef PROCESS_TYPE
         throw std::invalid_argument("Type not supported yet.");
@@ -53,33 +63,52 @@ struct CLAP {
     }
 
     static void parse_input(T& input, int argc, char** argv) {
-        (void)input;
-
+        std::size_t argument_idx = 0;
         for (int i = 1; i < argc; ++i) {
-            std::string option{argv[i]};
-            const auto& pm = get_parameter_map();
-            auto iter = pm.find(option);
-            if (iter == pm.end()) {
-                std::ostringstream oss;
-                oss << argv[0] << ": unrecognized option '" << option << "'\n";
-                throw std::invalid_argument(oss.str());
-            }
+            std::string option_or_arg{argv[i]};
+            if (option_or_arg[0] == '-') {
+                // it's an option
+                const auto& opts = get_options();
+                auto iter = opts.find(option_or_arg);
+                if (iter == opts.end()) {
+                    std::ostringstream oss;
+                    oss << argv[0] << ": unrecognized option '" << option_or_arg
+                        << "'\n";
+                    throw std::invalid_argument(oss.str());
+                }
 
-            // valid option
-            auto [offset, type_code] = iter->second;
-            std::string raw_value;
-            if (type_code == TYPE_CODE::bool_t) {
-                raw_value = "1";
-            } else if (i + 1 == argc) {
-                std::ostringstream oss;
-                oss << argv[0] << ": missing value of option '" << argv[i]
-                    << "'\n";
-                throw std::invalid_argument(oss.str());
+                // valid option
+                auto [offset, type_code] = iter->second;
+                std::string raw_value;
+                if (type_code == TYPE_CODE::bool_t) {
+                    raw_value = "1";
+                } else if (i + 1 == argc) {
+                    std::ostringstream oss;
+                    oss << argv[0] << ": missing value of option '"
+                        << option_or_arg << "'\n";
+                    throw std::invalid_argument(oss.str());
+                } else {
+                    raw_value = argv[++i];
+                }
+                assign_value(reinterpret_cast<char*>(&input) + offset,
+                             type_code, raw_value);
             } else {
-                raw_value = argv[++i];
+                // it's an argument
+                if (argument_idx == get_arguments().size()) {
+                    std::ostringstream oss;
+                    oss << argv[0] << ": too many arguments\n";
+                    throw std::invalid_argument(oss.str());
+                }
+                auto [offset, type_code] = get_arguments()[argument_idx];
+                assign_value(reinterpret_cast<char*>(&input) + offset,
+                             type_code, argv[i]);
+                ++argument_idx;
             }
-            assign_value(reinterpret_cast<char*>(&input) + offset, type_code,
-                         raw_value);
+        }
+        if (argument_idx < get_arguments().size()) {
+            std::ostringstream oss;
+            oss << argv[0] << ": too few arguments\n";
+            throw std::invalid_argument(oss.str());
         }
     }
 #undef TYPE_LIST
@@ -97,14 +126,14 @@ struct CLAP {
 
 #define CLAP_REGISTER_DIRECT(NAME)                                        \
     {                                                                     \
-        get_parameter_map().emplace(                                      \
+        get_options().emplace(                                            \
             "--" #NAME, std::make_pair(offsetof(base, NAME),              \
                                        get_type_code<decltype(NAME)>())); \
     }
 
 #define CLAP_REGISTER_EXTEND(NAME, OPTION_NAME)                            \
     {                                                                      \
-        get_parameter_map().emplace(                                       \
+        get_options().emplace(                                             \
             OPTION_NAME, std::make_pair(offsetof(base, NAME),              \
                                         get_type_code<decltype(NAME)>())); \
     }
@@ -112,8 +141,14 @@ struct CLAP {
 // Macro overloading depending on argument number
 // https://stackoverflow.com/a/11763277/7255197
 #define CLAP_GET_MACRO(_1, _2, NAME, ...) NAME
-#define CLAP_REGISTER(...)                                                    \
+#define CLAP_REGISTER_OPTION(...)                                             \
     CLAP_GET_MACRO(__VA_ARGS__, CLAP_REGISTER_EXTEND, CLAP_REGISTER_DIRECT, ) \
     (__VA_ARGS__)
+
+#define CLAP_REGISTER_ARG(NAME)                                        \
+    {                                                                  \
+        get_arguments().emplace_back(offsetof(base, NAME),             \
+                                     get_type_code<decltype(NAME)>()); \
+    }
 
 #endif  // ZQ_CLAP
