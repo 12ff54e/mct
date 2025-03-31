ccade1c1edd9a96119ea5a6d7cdcf5de90156116
qzhong <12ff54e@gmail.com>
2025-03-31 22:36:18 +0800

    Get rid of all warnings

--- lib/Timer.h
+++ lib/Timer.h
@@ -39,17 +39,17 @@ class Timer {
 #include <iomanip>
 #include <iostream>
 
-using namespace std::chrono;
-
 void Timer::start(std::string func_name) {
     current_func_name = func_name;
     auto emplace_reuslt = time_consuming.emplace(
-        func_name, std::make_pair(high_resolution_clock::duration::zero(),
-                                  high_resolution_clock::now()));
+        func_name,
+        std::make_pair(std::chrono::high_resolution_clock::duration::zero(),
+                       std::chrono::high_resolution_clock::now()));
     if (emplace_reuslt.second) {
         entries.push_back(func_name);
     } else {
-        emplace_reuslt.first->second.second = high_resolution_clock::now();
+        emplace_reuslt.first->second.second =
+            std::chrono::high_resolution_clock::now();
     }
 }
 
@@ -59,13 +59,13 @@ void Timer::pause_last_and_start_next(std::string func_name) {
 }
 
 void Timer::pause() {
-    auto end_time = high_resolution_clock::now();
+    auto end_time = std::chrono::high_resolution_clock::now();
     auto elapsed_time = end_time - time_consuming.at(current_func_name).second;
     time_consuming.at(current_func_name).first += elapsed_time;
 }
 
 void Timer::pause(std::string func_name) {
-    auto end_time = high_resolution_clock::now();
+    auto end_time = std::chrono::high_resolution_clock::now();
     auto elapsed_time = end_time - time_consuming.at(func_name).second;
     time_consuming.at(func_name).first += elapsed_time;
 }
@@ -76,9 +76,10 @@ void Timer::reset() {
 }
 
 void Timer::print() {
-    std::size_t max_length = 0;
+    int max_length = 0;
     for (auto& name : entries) {
-        max_length = max_length < name.size() ? name.size() : max_length;
+        const int length = static_cast<int>(name.size());
+        max_length = max_length < length ? length : max_length;
     }
 
     std::cout << '+' << std::setfill('-') << std::setw(max_length + 16) << '-'
@@ -92,11 +93,13 @@ void Timer::print() {
     std::cout << "+-------------+\n";
     for (const auto& name : entries) {
         auto& time_used = time_consuming.at(name).first;
-        std::cout << std::left << "| "
-                  << std::setw(static_cast<int>(max_length)) << name << " | "
-                  << std::right << std::setw(9)
-                  << duration<double, milliseconds::period>(time_used).count()
-                  << "ms |\n";
+        std::cout
+            << std::left << "| " << std::setw(static_cast<int>(max_length))
+            << name << " | " << std::right << std::setw(9)
+            << std::chrono::duration<double, std::chrono::milliseconds::period>(
+                   time_used)
+                   .count()
+            << "ms |\n";
     }
     std::cout << '+' << std::setfill('-') << std::setw(max_length + 2) << '-'
               << std::setfill(' ');
@@ -107,6 +110,5 @@ Timer& Timer::get_timer() {
     static Timer timer{};
     return timer;
 }
-
 #endif  // ZQ_TIMER_IMPLEMENTATION
 #endif  // ZQ_TIMER
